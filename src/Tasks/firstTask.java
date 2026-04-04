1894610ac092fa8bce1c163582058fd85a4c4381
Rafael <rafaelsaibro1@gmail.com>
2026-04-04 14:22:23 -0300

    Primitive and non-primitive data types

---
+++ src/Tasks/firstTask.java
@@ -0,0 +1,41 @@
+package Tasks;
+
+public class firstTask {
+    static void main(String[] args) {
+
+        String ninjaName1 = "Naruto";
+        int ninjaAge1 = 18;
+        String ninjaName2 = "Sasuke";
+        int ninjaAge2 = 20;
+
+        String taskOne = "Escort the villager through the forest";
+        String taskTwo = "Scout the hidden mist village area";
+        char taskOneLevel = 'A';
+        char taskTwoLevel = 'S';
+        String statusMission = "Mission";
+
+
+        System.out.println("Name: " + ninjaName1 + "\nAge: " + ninjaAge1 + "\nMission: " + taskOne + "\nMission Rank: " + taskOneLevel);
+        if (ninjaAge1 < 18 && taskOneLevel == 'A'){
+            System.out.println("Status: " + statusMission + " Failed");
+        } else {
+            System.out.println("Status: " + statusMission + " Completed");
+        }
+        System.out.println("\n------------------------------------\n");
+
+        System.out.println("Name: " + ninjaName2 + "\nAge: " + ninjaAge2 + "\nMission: " + taskTwo + "\nMission Rank: " + taskTwoLevel);
+
+        if(ninjaAge2 < 21 && taskTwoLevel == 'S'){
+            System.out.println("Status: " + statusMission +  " Failed");
+        } else {
+            System.out.println("Status: " + statusMission + " Completed");
+        }
+
+
+
+
+
+
+    }
+
+}
--- src/dataTypes/nonPrimitiveTypes.java
+++ src/dataTypes/nonPrimitiveTypes.java
@@ -2,7 +2,13 @@ package dataTypes;
 
 public class nonPrimitiveTypes {
     static void main(String[] args) {
-
+        /*
+        * String, Array, Class...
+        * */
+        String name = "Naruto";
+        String nameToUpperCase = name.toUpperCase();
+        String nameToLowerCase = name.toLowerCase();
+        System.out.println(name + "\n" + nameToUpperCase + "\n" + nameToLowerCase);
 
     }
 }
